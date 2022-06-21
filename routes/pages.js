4a3e5000b6bb4d5730b8e273c502c7cf15ccd136
josueroblesbus <josue.89.robles@gmail.com>
2022-06-21 18:09:53 -0500

    pages

--- routes/pages.js
+++ routes/pages.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const userController = require('../controllers/users');
+const almacen_Controller = require('../controllers/store');
 
 router.get(["/", "/login"], (req, res) => {
     // res.send("<h1>Hello Renzo</h1>")
@@ -79,5 +80,25 @@ router.get("/home", userController.isLoggedIn, (req, res) => {
         res.redirect("/login");
     }
 });
+
+//STORE-ALMACEN--JOSUE ROBLES 
+
+router.get("/busqueda",almacen_Controller.busqueda, (req, res) => {
+    res.render("busqueda");
+});
+router.post("/agregar", almacen_Controller.agregar, (req, res) => {
+    res.render("agregar");
+});
+//router.delete("/eliminar", almacen_Controller.eliminar, (req, res) => {
+//    res.render("eliminar");
+//});
+router.get("/actualizar",almacen_Controller.actualizar, (req, res) => {
+    res.render("actualizar");
+});
+router.get("/busqueda_id_piezas",almacen_Controller.busqueda_id_piezas, (req, res) => {
+    res.render("busqueda_id_piezas");
+});
+
+
 
 module.exports = router;
